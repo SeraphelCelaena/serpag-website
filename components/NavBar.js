0dddd6a99dbf61ab9841e59eebf1704b8e372316
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-11-25 03:17:27 -0700

    theme no workey :pleadingface:

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,22 +1,61 @@
+import {useState} from "react";
+
 import Appbar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
+
+import MenuIcon from '@mui/icons-material/Menu';
 
 import Link from "next/link";
+import Head from "next/head";
 
-export default function NavBar() {
-	return <Appbar position="static">
-		<Toolbar>
-			<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-				<Link href="/">Home</Link>
-			</Typography>
-			<Button color="inherit">
-				<Link href="/about">About</Link>
-			</Button>
-			<Button color="inherit">
-				<Link href="/contact">Contact</Link>
-			</Button>
-		</Toolbar>
-	</Appbar>
+const drawerWidth = 250;
+
+export default function NavBar({title}) {
+	const [drawerOpen, setDrawerOpen] = useState(false);
+
+	return <Box component="nav">
+		<Head>
+			<title>{title}</title>
+		</Head>
+		<Appbar position="static">
+			<Toolbar>
+				<IconButton
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="menu"
+					sx={{mr: 2}}
+					onClick={() => setDrawerOpen(true)}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Typography variant="h4">
+					<Link href="/">
+						Home
+					</Link>
+				</Typography>
+			</Toolbar>
+		</Appbar>
+		<Drawer
+			anchor="left"
+			open={drawerOpen}
+			sx={{
+				width: drawerWidth,
+				flexShrink: 0,
+				'& .MuiDrawer-paper': {width: drawerWidth, boxSizing: 'border-box'}
+			}}
+			onClose={() => setDrawerOpen(false)}
+			color="primary"
+		>
+			<Box
+				component="nav"
+			>
+
+			</Box>
+		</Drawer>
+	</Box>;
 }
