2431b5719e4af8b92bf82a95ba607d3be533c129
SeraphelCelaena <67750475+SeraphelCelaena@users.noreply.github.com>
2023-12-02 02:59:25 -0700

    document consistency

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,8 @@ import Typography from "@mui/material/Typography";
 import NavBar from "@/components/NavBar"
 
 export default function Home() {
-	return (
-		<main>
-			<NavBar title="Home Page" />
-			<Typography variant="h1">Banana</Typography>
-		</main>
-	);
+	return <main>
+		<NavBar title="Home Page" />
+		<Typography variant="h1">Banana</Typography>
+	</main>;
 }
